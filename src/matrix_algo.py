779b479af273b4514b3f9477aadf929b750fa5fc
AlexanderPolyakov <polyakov.alx@gmail.com>
2017-12-05 00:24:06 +0300

    Make some changes to prepare for adding test environment

--- src/matrix_algo.py
+++ src/matrix_algo.py
@@ -4,45 +4,56 @@ import matrix_closure as mc
 import argparse
 import copy
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--gram', dest='gram_path', type=str,
-                    help='Path to file with grammar')
-parser.add_argument('--graph', dest='graph_path', type=str,
-                    help='Path to file with graph/automaton')
-parser.add_argument('--out', dest='out_path', type=str,
-                    help='Path to file to store the results(optional)')
-args = parser.parse_args()
 
-grammar = gram.parse_grammar(args.gram_path)
-graph, N = gp.parse_graph(args.graph_path)
-matrix = [[[] for i in range(0, N)] for j in range(0, N)]
+def matrix_algorithm(gram_path, graph_path, out=None, test=False):
+    grammar = gram.parse_grammar(gram_path)
+    graph, N = gp.parse_graph(graph_path)
+    matrix = [[[] for i in range(0, N)] for j in range(0, N)]
 
-for (i, j, label) in graph:
-    for (left, right) in grammar:
-        if right == label:
-            matrix[int(i)][int(j)].append(left)
+    for (i, j, label) in graph:
+        for (left, right) in grammar:
+            if right == label:
+                matrix[int(i)][int(j)].append(left)
 
-old_matrix = ['!@#$%']
+    old_matrix = ['!@#$%']
 
-while old_matrix != matrix:
-    old_matrix = copy.deepcopy(matrix)
-    updated = mc.matrix_closure(copy.deepcopy(matrix), grammar, N)
+    while old_matrix != matrix:
+        old_matrix = copy.deepcopy(matrix)
+        updated = mc.matrix_closure(copy.deepcopy(matrix), grammar, N)
+        for i in range(0, N):
+            for j in range(0, N):
+                matrix[i][j] += updated[i][j]
+                matrix[i][j] = list(set(matrix[i][j]))
+
+    res = []
+    res_count = 0
+
     for i in range(0, N):
         for j in range(0, N):
-            matrix[i][j] += updated[i][j]
-            matrix[i][j] = list(set(matrix[i][j]))
+            if 'S' in matrix[i][j]:
+                res.append((i, 'S', j))
+                res_count += 1
 
-res = []
+    if test:
+        return res_count
+    else:
+        if out is None:
+            for (i, non_term, j) in res:
+                print(str(i) + ',' + non_term + ',' + str(j))
+        else:
+            with open(args.out_path, 'w') as f:
+                for (i, non_term, j) in res:
+                    f.write(str(i) + ',' + non_term + ',' + str(j) + '\n')
 
-for i in range(0, N):
-    for j in range(0, N):
-        if 'S' in matrix[i][j]:
-            res.append((i, 'S', j))
+if __name__ == '__main__':
 
-if args.out_path is None:
-    for (i, non_term, j) in res:
-        print(str(i) + ',' + non_term + ',' + str(j))
-else:
-    with open(args.out_path, 'w') as f:
-        for (i, non_term, j) in res:
-            f.write(str(i) + ',' + non_term + ',' + str(j) + '\n')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gram', dest='gram_path', type=str,
+                        help='Path to file with grammar')
+    parser.add_argument('--graph', dest='graph_path', type=str,
+                        help='Path to file with graph/automaton')
+    parser.add_argument('--out', dest='out_path', type=str,
+                        help='Path to file to store the results(optional)')
+    args = parser.parse_args()
+
+    matrix_algorithm(args.gram_path, args.graph_path, args.out_path)
