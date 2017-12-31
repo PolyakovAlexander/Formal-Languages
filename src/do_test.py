d31644873528df144e1b4286ae752122c1369f31
AlexanderPolyakov <polyakov.alx@gmail.com>
2017-12-31 03:15:30 +0300

    Small fixes and changes

--- src/do_test.py
+++ src/do_test.py
@@ -1,51 +1,125 @@
-import os
-from collections import defaultdict
-from src.matrix_algo import matrix_algo as ma
-from src.bottom_up_algo import bottom_up_algo as bot_up
 import sys
+import matrix_algo
+import bottom_up
+import top_down
+
+test_data_Q1 = {
+    'skos.dot': 810,
+    'generations.dot' : 2164,
+    'travel.dot' : 2499,
+    'univ-bench.dot' : 2540,
+    'atom-primitive.dot' : 15454,
+    'biomedical-mesure-primitive.dot' : 15156,
+    'foaf.dot' : 4118,
+    'people_pets.dot' : 9472,
+    'funding.dot' : 17634,
+    'wine.dot' : 66572,
+    'pizza.dot' : 56195,
+}
+
+test_data_Q2 = {
+    'skos.dot': 1,
+    'generations.dot' : 0,
+    'travel.dot' : 63,
+    'univ-bench.dot' : 81,
+    'atom-primitive.dot' : 122,
+    'biomedical-mesure-primitive.dot' : 2871,
+    'foaf.dot' : 10,
+    'people_pets.dot' : 37,
+    'funding.dot' : 1158,
+    'wine.dot' : 133,
+    'pizza.dot' : 1262,
+}
+
+custom_test_res = [
+    (0, 'S', 1), (0, 'S', 3), (0, 'S', 5), (0, 'S', 7),
+    (1, 'S', 2), (1, 'S', 4), (1, 'S', 6), (2, 'S', 3),
+    (2, 'S', 5), (2, 'S', 7), (3, 'S', 4), (3, 'S', 6),
+    (4, 'S', 5), (4, 'S', 7), (5, 'S', 6), (6, 'S', 7)
+]
 
 
-def test(grammar_type, algo):
+def test_matrix_method(test_data, grammar_name):
+    print('Matrix analysis...')
+    graphs_path = 'data/test_graphs/'
+    grammars_path = 'data/test_grammars/'
+    for name, res in test_data.items():
+        print ("Test for " + grammar_name + " and " + name + " started.")
+        leng = matrix_algo.matrix_algorithm(graphs_path + name, grammars_path + grammar_name, test=True)
+        if leng != res:
+            print (name + " failed")
+            return False
+        else:
+            print("Test for " + grammar_name + " and " + name + " completed successfully.")
 
-    src_path = os.path.abspath(os.path.dirname(__file__))
-    test_data = src_path + '/data/'
-    grammars_path = test_data + 'test_grammars/'
-    graphs_path = test_data + 'test_graphs/'
-    check_table_path = test_data + 'check_table'
 
-    test_counter = 0
-    grammars = list(filter(lambda x: grammar_type in x, os.listdir(grammars_path)))
-    grammars.remove('q3_' + grammar_type)
-    graphs = os.listdir(graphs_path)
-    check_table = defaultdict(list)
-    with open(check_table_path, 'r') as f:
-        for line in f:
-            graph_name, q1_res, q2_res = map(lambda x: x.replace('\n', ''), line.split(';'))
-            check_table[graph_name] = [('q1_' + grammar_type, q1_res), ('q2_' + grammar_type, q2_res)]
+def test_bottom_up_method(test_data, grammar_name):
+    print('Bottom-up analysis...')
+    graphs_path = 'data/test_graphs/'
+    grammars_path = 'data/test_grammars/'
+    for name, res in test_data.items():
+        print("Test for " + grammar_name + " and " + name + " started.")
+        leng = bottom_up.bottom_up_algo(graphs_path + name, grammars_path + grammar_name, test=True)
+        if leng != res:
+            print(name + " failed")
+            return False
+        else:
+            print("Test for " + grammar_name + " and " + name + " completed successfully.")
 
-    if algo == 'first':
-        for grammar in grammars:
-            for graph in graphs:
-                print(grammar + ' / ' + graph + ' test: ')
-                if (grammar, str(ma.matrix_algorithm(grammars_path + grammar,
-                                                     graphs_path + graph, test=True))) in check_table.get(graph, []):
-                    print('PASSED')
-                    test_counter += 1
-                else:
-                    print('FAILED')
-    elif algo == 'second':
-        for grammar in grammars:
-            for graph in graphs:
-                print(grammar + ' / ' + graph + ' test: ')
-                if (grammar, str(bot_up.bottom_up_algo(grammars_path + grammar,
-                                                     graphs_path + graph, test=True))) in check_table.get(graph, []):
-                    print('PASSED')
-                    test_counter += 1
-                else:
-                    print('FAILED')
 
-    print(str(test_counter) + '/' + str(len(check_table) * 2) + ' tests passed')
+def test_top_down_method(test_data, grammar_name):
+    print('Top-down analysis...')
+    graphs_path = 'data/test_graphs/'
+    grammars_path = 'data/test_grammars/'
+    for name, res in test_data.items():
+        print("Test for " + grammar_name + " and " + name + " started.")
+        leng = top_down.top_down(graphs_path + name, grammars_path + grammar_name, test=True)
+        if leng != res:
+            print (name + " failed")
+            return False
+        else:
+            print("Test for " + grammar_name + " and " + name + " completed successfully.")
+
 
 if __name__ == '__main__':
 
-    test(sys.argv[1], sys.argv[2])
+        matrix_custom = matrix_algo.matrix_algorithm('data/test_graphs/my_graph',
+                                                     'data/test_grammars/my_grammar_chomsky',
+                                                     custom_test=True).sort() == custom_test_res.sort()
+        if matrix_custom:
+            print('Custom test WWr passed with matrix algorithm on 1010101')
+        else:
+            print('Custom test WWr failed with matrix algorithm on 1010101')
+
+
+        bottom_up_custom = bottom_up.bottom_up_algo('data/test_graphs/my_graph',
+                                                    'data/test_grammars/my_grammar_automata',
+                                                    custom_test=True).sort() == custom_test_res.sort()
+        if bottom_up_custom:
+            print('Custom test WWr passed with bottom-up algorithm on 1010101')
+        else:
+            print('Custom test WWr failed with bottom-up algorithm on 1010101')
+
+
+        top_down_custom = top_down.top_down('data/test_graphs/my_graph',
+                                            'data/test_grammars/my_grammar_automata',
+                                            custom_test=True).sort() == custom_test_res.sort()
+        if top_down_custom:
+            print('Custom test WWr passed with top-down algorithm on 1010101')
+        else:
+            print('Custom test WWr failed with top-down algorithm on 1010101')
+
+        q1_test = test_matrix_method(test_data_Q1, 'q1_chomsky')
+        q2_test = test_matrix_method(test_data_Q2, 'q2_chomsky')
+        if q1_test and q2_test:
+            print("All tests for matrix method passed successfully")
+
+        q1_test = test_bottom_up_method(test_data_Q1, 'q1')
+        q2_test = test_bottom_up_method(test_data_Q2, 'q2')
+        if q1_test and q2_test:
+            print("All tests for bottom-up method passed successfully")
+
+        q1_test = test_top_down_method(test_data_Q1, 'q1')
+        q2_test = test_top_down_method(test_data_Q2, 'q2')
+        if q1_test and q2_test:
+            print("All tests for top-down method passed successfully")
