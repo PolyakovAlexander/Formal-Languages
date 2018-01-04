7058cefe4b65f53d3c361307b5d7f49d31bb162e
AlexanderPolyakov <polyakov.alx@gmail.com>
2018-01-04 20:15:01 +0300

    Update custom test

--- do_test.py
+++ do_test.py
@@ -32,10 +32,9 @@ test_data_Q2 = {
 }
 
 custom_test_res = [
-    (0, 'S', 1), (0, 'S', 3), (0, 'S', 5), (0, 'S', 7),
-    (1, 'S', 2), (1, 'S', 4), (1, 'S', 6), (2, 'S', 3),
-    (2, 'S', 5), (2, 'S', 7), (3, 'S', 4), (3, 'S', 6),
-    (4, 'S', 5), (4, 'S', 7), (5, 'S', 6), (6, 'S', 7)
+    (0, 'S', 0),
+    (0, 'S', 1),
+    (1, 'S', 1)
 ]
 
 
@@ -83,43 +82,46 @@ def test_top_down_method(test_data, grammar_name):
 
 if __name__ == '__main__':
 
-        matrix_custom = matrix_algo.matrix_algorithm('data/test_graphs/my_graph',
-                                                     'data/test_grammars/my_grammar_chomsky',
-                                                     custom_test=True).sort() == custom_test_res.sort()
-        if matrix_custom:
-            print('Custom test WWr passed with matrix algorithm on 1010101')
-        else:
-            print('Custom test WWr failed with matrix algorithm on 1010101')
+    print('Custom graph:\n0 -> 0[label="1"]\n0 -> 1[label="0"]\n'
+          '1 -> 1[label"1"]\n1 -> 1[label="0"]\n')
+
+    matrix_custom = matrix_algo.matrix_algorithm('data/test_graphs/my_graph',
+                                                 'data/test_grammars/my_grammar_chomsky',
+                                                 custom_test=True).sort() == custom_test_res.sort()
+    if matrix_custom:
+        print('Custom test WWr passed with matrix algorithm')
+    else:
+        print('Custom test WWr failed with matrix algorithm')
 
 
-        bottom_up_custom = bottom_up.bottom_up_algo('data/test_graphs/my_graph',
-                                                    'data/test_grammars/my_grammar_automata',
-                                                    custom_test=True).sort() == custom_test_res.sort()
-        if bottom_up_custom:
-            print('Custom test WWr passed with bottom-up algorithm on 1010101')
-        else:
-            print('Custom test WWr failed with bottom-up algorithm on 1010101')
+    bottom_up_custom = bottom_up.bottom_up_algo('data/test_graphs/my_graph',
+                                                'data/test_grammars/my_grammar_automata',
+                                                custom_test=True).sort() == custom_test_res.sort()
+    if bottom_up_custom:
+        print('Custom test WWr passed with bottom-up algorithm')
+    else:
+        print('Custom test WWr failed with bottom-up algorithm')
 
 
-        top_down_custom = top_down.top_down('data/test_graphs/my_graph',
-                                            'data/test_grammars/my_grammar_automata',
-                                            custom_test=True).sort() == custom_test_res.sort()
-        if top_down_custom:
-            print('Custom test WWr passed with top-down algorithm on 1010101')
-        else:
-            print('Custom test WWr failed with top-down algorithm on 1010101')
+    top_down_custom = top_down.top_down('data/test_graphs/my_graph',
+                                        'data/test_grammars/my_grammar_automata',
+                                        custom_test=True).sort() == custom_test_res.sort()
+    if top_down_custom:
+        print('Custom test WWr passed with top-down algorithm')
+    else:
+        print('Custom test WWr failed with top-down algorithm')
 
-        q1_test = test_matrix_method(test_data_Q1, 'q1_chomsky')
-        q2_test = test_matrix_method(test_data_Q2, 'q2_chomsky')
-        if q1_test and q2_test:
-            print("All tests for matrix method passed successfully")
+    q1_test = test_matrix_method(test_data_Q1, 'q1_chomsky')
+    q2_test = test_matrix_method(test_data_Q2, 'q2_chomsky')
+    if q1_test and q2_test:
+        print("All tests for matrix method passed successfully")
 
-        q1_test = test_bottom_up_method(test_data_Q1, 'q1')
-        q2_test = test_bottom_up_method(test_data_Q2, 'q2')
-        if q1_test and q2_test:
-            print("All tests for bottom-up method passed successfully")
+    q1_test = test_bottom_up_method(test_data_Q1, 'q1')
+    q2_test = test_bottom_up_method(test_data_Q2, 'q2')
+    if q1_test and q2_test:
+        print("All tests for bottom-up method passed successfully")
 
-        q1_test = test_top_down_method(test_data_Q1, 'q1')
-        q2_test = test_top_down_method(test_data_Q2, 'q2')
-        if q1_test and q2_test:
-            print("All tests for top-down method passed successfully")
+    q1_test = test_top_down_method(test_data_Q1, 'q1')
+    q2_test = test_top_down_method(test_data_Q2, 'q2')
+    if q1_test and q2_test:
+        print("All tests for top-down method passed successfully")
